80e860662bb8f256ae6d4fae6888b20d00b9131c
Regina Junkina <stupidfactory@mail.ru>
2017-03-15 12:37:12 +0200

    Add files via upload

--- src/main/java/regina/Rakendus.java
+++ src/main/java/regina/Rakendus.java
@@ -9,11 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	
 	@RequestMapping("/geomeetriline_keskmine")
-	String gk(String arv1, String arv2){
+	String gk(String arv1, String arv2, String arv3){
 		if(arv1==null){return "andmed puuduvad";}
 		int a1=Integer.parseInt(arv1);
 		int a2=Integer.parseInt(arv2);
-		return arv1+" ja "+arv2+ " geomeetriline keskmine on" + Math.sqrt(a1*a2);
+		int a3=Integer.parseInt(arv3);
+		return arv1+" ja "+arv2+" ja "+ arv3+ " geomeetriline keskmine on " + Math.pow(a1*a2*a3, 1.0/3);
+	}
+	
+	@RequestMapping("/hinnakalkulaator")
+	String kk(String kogus){
+			if(kogus==null){return "sisesta kogus"}
+			int k=Integer.parseInt(kogus);
+			if(kogus<5){return kogus+"tükki hinnaks on "+ kogus*3+ " eur"}
+			if(kogus>=5){return kogus+"tükki hinnaks on "+ kogus*2+ " eur"}
+			
 	}
 	
 	@RequestMapping("/algus")
@@ -33,7 +43,7 @@ public class Rakendus {
 	
 	
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 4201);
+		System.getProperties().put("server.port", 4201);
         SpringApplication.run(Rakendus.class, args);
 		
     }
